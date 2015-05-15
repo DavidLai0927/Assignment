7b5f85a77dfc4d1fcfbcfce73800425ec3eb20b0
DavidLai0927 <B22475656@gmail.com>
2015-05-15 13:44:06 +0800

    0515 C# Assignment9 without bonus

---
+++ C_SharpAssignment/C_Sharp_Assignment9/Mod_9_Homework/Mod_9_Homework/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mod_9_Homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Student> studentList = new List<Student>();
+        int indexOfList = 0;
+        private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student();
+            student.firstName = txtFirstName.Text;
+            student.lastName = txtLastName.Text;
+            student.city = txtCity.Text;
+            studentList.Add(student);
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtCity.Clear();
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            if (indexOfList > 0)
+            {
+                indexOfList--;
+                txtFirstName.Text = studentList[indexOfList].firstName;
+                txtLastName.Text = studentList[indexOfList].lastName;
+                txtCity.Text = studentList[indexOfList].city;
+                if (indexOfList == 0)
+                    btnPrevious.IsEnabled = false;
+            }
+            if (btnNext.IsEnabled == false)
+                btnNext.IsEnabled = true;
+            
+            
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (indexOfList < studentList.Count-1)
+            {
+                indexOfList++;
+                txtFirstName.Text = studentList[indexOfList].firstName;
+                txtLastName.Text = studentList[indexOfList].lastName;
+                txtCity.Text = studentList[indexOfList].city;
+                if (indexOfList == studentList.Count - 1)
+                    btnPrevious.IsEnabled = false;
+            }
+            if (btnPrevious.IsEnabled == false)
+                btnPrevious.IsEnabled = true;
+        }
+    }
+
+    public class Student {
+        public string firstName,lastName,city;
+        public Student()
+        {
+        }
+        public Student(string firstName,string lastName,string city) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.city = city;
+        }
+    }
+}
