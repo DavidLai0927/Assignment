fbc9cf84460cc191be128de59fd6da1178fba534
DavidLai0927 <B22475656@gmail.com>
2015-06-05 10:52:12 +0800

    Revise progrom.cs

--- C_SharpAssignment/Calculator/Calculator/Program.cs
+++ C_SharpAssignment/Calculator/Calculator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +10,10 @@ namespace Calculator
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            
+
             Console.WriteLine("C# Simple Console Calculator");
             Console.WriteLine("Type \"Quit\" to quit");
             myCal cal = new myCal();
@@ -24,19 +24,41 @@ namespace Calculator
                 cal.read(input);
             }
         }
-        
+
     }
 
     class myCal
     {
         Stack<char> myOp = new Stack<char>();
         Stack<double> myNum = new Stack<double>();
-        double l, r;
+        double l, r, temp = 0, decTemp = 1, dec = 0;
         char buffer = 'n';
         public void read(char input)
         {
             try
             {
+                if (buffer != 'n')
+                {
+                    if (input != '.' && ((Convert.ToInt32(input) - '0') < 0 || (Convert.ToInt32(input) - '0') > 9))
+                    {
+                        temp += dec;
+                        myNum.Push(temp);
+                        dec = 0;
+                        temp = 0;
+                        decTemp = 1;
+                        buffer = 'n';
+                    }
+                    else if (buffer == '.')
+                    {
+                        decTemp /= 10;
+                        dec += (Convert.ToInt32(input) - '0') * decTemp;
+                    }
+                    else if (input != '.')
+                    {
+                        temp = temp * 10 + (Convert.ToInt32(input) - '0');
+                    }
+
+                }
                 if (input == '+' || input == '-')
                 {
                     while (myOp.Count() != 0 && myOp.Peek() != '(')
@@ -64,11 +86,11 @@ namespace Calculator
                 {
                     myOp.Push(input);
                 }
-                else if (input == ')') 
+                else if (input == ')')
                 {
                     while (myOp.Peek() != '(')
                     {
-                        if (myOp.Count()==0)
+                        if (myOp.Count() == 0)
                         {
                             throw new Exception("");
                         }
@@ -95,7 +117,7 @@ namespace Calculator
                 {
                     read('+');
                     myOp.Pop();
-                    if (myOp.Count != 0 )
+                    if (myOp.Count != 0)
                         throw new Exception("");
                     if (myNum.Count != 0)
                     {
@@ -110,12 +132,19 @@ namespace Calculator
 
                     myOp.Clear();
                     myNum.Clear();
-                    
+
                 }
                 else if (input == '\t' || input == ' ' || input == '\r')
                 {
                 }
-    
+                else if (input == '.' || ((Convert.ToInt32(input) - '0') >= 0 && (Convert.ToInt32(input) - '0') <= 9))
+                {
+                    if (buffer == 'n')
+                        temp = (Convert.ToInt32(input) - '0');
+                    if (buffer != '.')
+                        buffer = input;
+
+                }
                 else if (input == 'q' || input == 'Q')
                 {
                     System.Environment.Exit(0);
@@ -124,9 +153,9 @@ namespace Calculator
                 {
                     throw new Exception("");
                 }
-     
-                
-                    
+
+
+
             }
             catch (Exception e)
             {
@@ -137,7 +166,7 @@ namespace Calculator
                 Console.Write("> ");
             }
         }
-       
-      }
+
+    }
 
 }
